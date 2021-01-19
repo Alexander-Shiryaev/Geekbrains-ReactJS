4189be387b99ca30aab59a7e4f8bb3628fe9df3c
Shiryaev Alexander <alex_sh_v@mail.ru>
2021-01-19 14:14:40 +0300

    fixed hw lesson2

--- static_src/components/Message.jsx
+++ static_src/components/Message.jsx
@@ -1,18 +1,8 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-export default class Message extends React.Component {
-    static propTypes = {
-        text: PropTypes.string.isRequired,
-    };
+export default function Message({ message }) {
 
-    render() {
-        return (
-            <div>
-                <div className='message-item'>{this.props.author}</div>
-                <div className='message-item'>{this.props.text}</div>
-            </div>
-
-        )
-    }
+    return (
+        <div className='message-item'>{message.author}: {message.text}</div>
+    )
 }
