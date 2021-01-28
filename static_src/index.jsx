d5a173f98e899fe3803278514fcacbb3d8e54a1c
Alexander <alex_sh_v@mail.ru>
2021-01-28 20:21:30 +0300

    fixed routes

--- static_src/index.jsx
+++ static_src/index.jsx
@@ -1,14 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Router from './components/Router'
-import { HashRouter } from 'react-router-dom';
+import Router from './components/Router';
+import { BrowserRouter } from 'react-router-dom';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 ReactDOM.render(
-   <HashRouter>
+   <BrowserRouter>
        <MuiThemeProvider>
            <Router />
        </MuiThemeProvider>
-   </HashRouter>,
+   </BrowserRouter>,
    document.getElementById('root'),
 );
