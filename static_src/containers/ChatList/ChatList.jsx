a8db7c6cc78c68c5501ff51b7079cd597b416f85
Alexander <alex_sh_v@mail.ru>
2021-02-01 14:57:58 +0300

    added connected-react-router

--- static_src/containers/ChatList/ChatList.jsx
+++ static_src/containers/ChatList/ChatList.jsx
@@ -1,80 +1,88 @@
-import React from 'react';
-import {bindActionCreators} from "redux";
+import React from "react";
+import { bindActionCreators } from "redux";
 import connect from "react-redux/es/connect/connect";
-import { Link } from 'react-router-dom'
-import { List, ListItem } from 'material-ui/List';
-import { TextField } from 'material-ui';
-import AddIcon from 'material-ui/svg-icons/content/add';
-import ContentSend from 'material-ui/svg-icons/content/send';
+import { push } from "connected-react-router";
+import { List, ListItem } from "material-ui/List";
+import { TextField } from "material-ui";
+import AddIcon from "material-ui/svg-icons/content/add";
+import ContentSend from "material-ui/svg-icons/content/send";
 import PropTypes from "prop-types";
-import { addChat } from '../../actions/chatActions';
-import './ChatList';
-
+import { addChat } from "../../actions/chatActions";
+import "./ChatList";
 
 class ChatList extends React.Component {
-   static propTypes = {
-       chats: PropTypes.object.isRequired,
-       addChat: PropTypes.func.isRequired,
-   };
+  static propTypes = {
+    chats: PropTypes.object.isRequired,
+    addChat: PropTypes.func.isRequired,
+    push: PropTypes.func.isRequired,
+  };
 
-   state = {
-       input: '',
-   };
+  state = {
+    input: "",
+  };
 
-   handleChange = (event) => {
-       this.setState({ [event.target.name]: event.target.value });
-   };
+  handleChange = (event) => {
+    this.setState({ [event.target.name]: event.target.value });
+  };
 
-   handleKeyUp = (event) => {
-       if (event.keyCode === 13) { // Enter
-           this.handleAddChat();
-       }
-   };
+  handleKeyUp = (event) => {
+    if (event.keyCode === 13) {
+      // Enter
+      this.handleAddChat();
+    }
+  };
 
-   handleAddChat = () => {
-       if (this.state.input.length > 0) {
-           this.props.addChat(this.state.input);
-           this.setState({ input: '' });
-       }
-   };
+  handleAddChat = () => {
+    if (this.state.input.length > 0) {
+      this.props.addChat(this.state.input);
+      this.setState({ input: "" });
+    }
+  };
 
-   render() {
-       const { chats } = this.props;
-       const chatElements = Object.keys(chats).map(chatId => (
-           <Link key={ chatId } to={ `/chat/${chatId}` }>
-               <ListItem
-                   primaryText={ chats[chatId].title }
-                   leftIcon={ <ContentSend /> }
-               />
-           </Link>));
+  handleNavigate = (link) => {
+    this.props.push(link);
+  };
 
-       return (
-           <List>
-               { chatElements }
-               <ListItem
-                   key="Add new chat"
-                   leftIcon={ <AddIcon /> }
-                   onClick={ this.handleAddChat }
-                   style={ { height: '60px' } }
-                   children= {<TextField
-                       key="textField"
-                       fullWidth
-                       name="input"
-                       hintText="Добавить новый чат"
-                       onChange={ this.handleChange }
-                       value={ this.state.input }
-                       onKeyUp={ this.handleKeyUp }
-                   />}
-               />
-           </List>
-       )
-   }
+  render() {
+    const { chats } = this.props;
+    const chatElements = Object.keys(chats).map((chatId) => (
+        <ListItem
+          primaryText={chats[chatId].title}
+          leftIcon={<ContentSend />}
+          onClick={ () => this.handleNavigate(`/chat/${chatId}`) }
+        />
+    ));
+
+    return (
+      <List>
+        {chatElements}
+        <ListItem
+          key="Add new chat"
+          leftIcon={<AddIcon />}
+          onClick={this.handleAddChat}
+          style={{ height: "60px" }}
+          children={
+            <TextField
+              key="textField"
+              fullWidth
+              name="input"
+              hintText="Добавить новый чат"
+              onChange={this.handleChange}
+              value={this.state.input}
+              onKeyUp={this.handleKeyUp}
+            />
+          }
+        />
+      </List>
+    );
+  }
 }
 
 const mapStateToProps = ({ chatReducer }) => ({
-    chats: chatReducer.chats,
- });
- 
- const mapDispatchToProps = dispatch => bindActionCreators({ addChat }, dispatch);
- 
- export default connect(mapStateToProps, mapDispatchToProps)(ChatList);
+  chats: chatReducer.chats,
+});
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators({ addChat, push }, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(ChatList);
