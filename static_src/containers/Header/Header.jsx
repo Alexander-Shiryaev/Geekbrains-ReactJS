a089ce828484cfd6890c32312dcd2f877a9d2f68
Alexander <alex_sh_v@mail.ru>
2021-02-17 19:08:17 +0300

    commit1 hw8

--- static_src/containers/Header/Header.jsx
+++ static_src/containers/Header/Header.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
+import { userName } from "../Profile/Profile";
 import "./Header.scss";
 
 export default class Header extends React.Component {
@@ -13,14 +14,14 @@ export default class Header extends React.Component {
     chatId: 1,
     profileId: 2,
   };
+  
 
   render() {
     return (
       <header className="header">
-        <Link to="/profile/">
-          <span>profile</span>
-        </Link>
+        <Link to="/profile/">profile</Link>
         <span>Чат {this.props.chatId}</span>
+        {userName}
       </header>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -31,7 +31,7 @@ module.exports = {
     },
     devServer: {
         port: 8080,
-        historyApiFallback: true
+        historyApiFallback: true,
     },
     // devtool: 'cheap-inline-module-source-map',
 };
